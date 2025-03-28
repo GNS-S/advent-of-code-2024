dd344309b50754077299dd254fa7f44d645a804e
GNS-S <samoilovas.ignas@gmail.com>
2025-03-28 22:54:04 +0200

    25 simplify

--- 25/part1.py
+++ 25/part1.py
@@ -2,34 +2,24 @@ def main():
     INPUT_PATH = './input.txt'
 
     with open(INPUT_PATH) as f:
-        keys, locks, height = parse_input(f)
+        keys, locks = parse_input(f)
 
         s = 0
         for key in keys:
             for lock in locks:
-                if all([key[i] + lock[i] <= height for i in range(height)]):
+                if all([key[i] != '#' or lock[i] != '#' for i in range(len(key))]):
                     s += 1
         
         print(s)
 
 def parse_input(file) -> dict[str, set]:
     schematics = file.read().strip().split('\n\n')
-    keys, locks, height = [], [], 0
+    keys, locks = [], []
 
     for s in schematics:
-        group = keys if s[0] == '.' else locks
-        weights = [0, 0, 0, 0, 0]
+        (keys if s[0] == '.' else locks).append(s)
 
-        lines = s.splitlines()
-        height = max(len(lines) - 2, height)
-
-        for line in lines[1:len(lines) -1]:
-            for i, c in enumerate(line):
-                if c == '#': weights[i] += 1
-        
-        group.append(weights)
-    
-    return keys, locks, height
+    return keys, locks
 
 if __name__ == '__main__':
     main()
